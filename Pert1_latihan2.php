2e76b3eed263735233fd5093cf03c24accbd39b2
Ananta <79901644+AnantaPrasetiyanto@users.noreply.github.com>
2021-03-02 19:10:50 +0700

    Add files via upload

---
+++ Pert1_latihan2.php
@@ -0,0 +1,12 @@
+<?php  
+	/*ANANTA PRASETIYANTO*/
+	$A = 123;
+	function test(){
+		global $A;
+		echo "Nilai A dalam fungsi = $A </br>";
+	}
+	test();
+	echo "Nilai A luar fungsi = $A </br>";
+
+
+?>
