fe3b147070d51afe121bc6dc2ffe0c06ffd8ae94
Ananta <79901644+AnantaPrasetiyanto@users.noreply.github.com>
2021-03-16 18:03:17 +0700

    Add files via upload

---
+++ Gambar Pert 7/Pert7_latihan4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penggunaan Is Array</title>
+</head>
+<body>
+	<H1>ANANTA PRASETIYANTO</H1>
+
+	<?php 
+	$var = array(1,2,3,4,5,6,7);
+	$scan = is_array($var);
+	if ($scan === false) {
+		$status = "bukan";
+	}else{
+		$status = "";
+	}
+	echo "\$var = array(1,2,3,4,5,6,7)";
+	echo "<br>";
+	echo "Variable \$var $status merupakan array";
